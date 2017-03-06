298ac26b91327b2bc4f2393646da96200c36cc28
Vitaliy Tkach <agrozapchasti@gmail.com>
2017-03-06 21:49:30 +0200

    Homework JS 1-2 corrections

--- Homework JS 1-2/js/names.js
+++ Homework JS 1-2/js/names.js
@@ -3,20 +3,23 @@
 var names = ['', '', '', '', ''];
 
 function namesButtonClicked() {  
-	for (var i = 0; i < 5; i++) {
-		names[i] = prompt('Введите ' + i + ' имя ', '');
-	}
-	
-	var userName = prompt('Как Вас зовут?', '');
+  for (var i = 0; i < 5; i++) {
+    names[i] = prompt('Введите ' + i + ' имя ', '');
+  }
+}
 
-	for (var i = 0; i < 5; i++) {
-		if (names[i] == userName) {
-			alert(userName + ' Вы успешно вошли!');
-		return;    
-		}
-	}
-		alert('ERROR!' + '\n' +
-			'Извените, Введенное имя пользователя не существует!');
-		return;
-	}
-	
+function enterButtonClicked() {
+  userName = prompt('Как Вас зовут?', '');
+
+  for (var i = 0; i < 5; i++) {
+    if (names[i] == userName) {
+			alert(userName + ' Вы успешно вошли!');	
+      return;    
+    }
+  }
+
+  alert('ERROR!' + '\n' +
+        'Извените, Введенное имя пользователя не существует!');
+  return;
+}
+
